979c4e392228bac32e360563954bf14c9bb463d3
Trian <triannurizkillah@gmail.com>
2019-10-07 21:21:50 +0700

    add

--- config/connection.js
+++ config/connection.js
@@ -14,3 +14,19 @@ con.connect(function(err) {
 });
 
 module.exports = con;
+// const mysql = require("mysql");
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "projekus_trian",
+//   password: "asdf1998Buka",
+//   database: "projekus_islamic_center",
+//   port: 3306
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("connect");
+// });
+
+// module.exports = con;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const routes = require("./lib/routes");
 
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
 routes(app);
 app.listen(port, (stat, err) => {
   console.log("localhost:" + port);
