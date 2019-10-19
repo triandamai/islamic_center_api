8e833f76ef1e835d4d6ac6126e3c4d47602d0310
Trian <triannurizkillah@gmail.com>
2019-10-19 16:17:27 +0700

    some new

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ const app = express();
 const port = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const routes = require("./lib/routes");
+const serveIndex = require("serve-index");
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
+//app.use(express.json({ limit: "50mb" }));
+//app.use(express.urlencoded({ limit: "50mb" }));
+
+app.use(
+  "/ftp",
+  express.static("public"),
+  serveIndex("public", { icons: true })
+);
 routes(app);
 app.listen(port, (stat, err) => {
   console.log("localhost:" + port);
